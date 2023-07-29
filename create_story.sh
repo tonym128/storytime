a66d309602d5e256545a1d169de3aa0b60b5d7c7
Tony Mamacos <tmamacos@gmail.com>
2023-07-29 19:59:56 +0100

    New Story

--- create_story.sh
+++ create_story.sh
@@ -4,7 +4,7 @@ dir=$PWD
 
 cd ../llama2.c
 ./run out44m/model44m.bin
-cp output.txt "../$dir/output.txt"
+cp output.txt "$dir/output.txt"
 cd "$dir"
 python ./create_post.py --content output.txt
 git add *
